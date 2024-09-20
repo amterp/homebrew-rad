f5fccfd9657f515e905dc21e0bfb35a04c894b74
Alexander Terp <alexander.terp@gmail.com>
2024-09-20 21:28:53 +1000

    Update username to amterp

--- rad.rb
+++ rad.rb
@@ -1,7 +1,7 @@
 class Rad < Formula
   desc "CLI tool for JSON API query scripting using RSL (Rad Scripting Language)"
-  homepage "https://github.com/AlexanderTerp/rad"
-  url "https://github.com/AlexanderTerp/rad/archive/0.2.7.tar.gz"
+  homepage "https://github.com/amterp/rad"
+  url "https://github.com/amterp/rad/archive/0.2.7.tar.gz"
   sha256 "2eca4c68d41ccc7c0cfd444feaa1b0caeafdd056016fb8d11293ce3777dbc21d"
   
   depends_on "go" => :build
