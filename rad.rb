0372c210d1f8ddcee08281879dd9f7b4da10f05b
Alexander Terp <alexander.terp@gmail.com>
2024-10-22 11:10:37 +0000

    rad 0.3.13

--- rad.rb
+++ rad.rb
@@ -1,8 +1,8 @@
 class Rad < Formula
   desc "CLI tool for JSON API query scripting using RSL (Rad Scripting Language)"
   homepage "https://github.com/amterp/rad"
-  url "https://github.com/amterp/rad/archive/0.3.12.tar.gz"
-  sha256 "b78940eef3c21ca439bb5516ff6cb20922eff8a40b3ae9bd63c10ab562e6c381"
+  url "https://github.com/amterp/rad/archive/0.3.13.tar.gz"
+  sha256 "6870769ecf7122fdd8a84cdaed13e55e024ffdcb7a728611abd519927268d121"
   
   depends_on "go" => :build
   
