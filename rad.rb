ba1cf13e24576f88e97999afb4064dede9cc26cf
Alexander Terp <alexander.terp@gmail.com>
2025-04-30 20:52:45 +1000

    Update rad.rb

--- rad.rb
+++ rad.rb
@@ -1,5 +1,5 @@
 class Rad < Formula
-  desc "CLI tool for JSON API query scripting using RSL (Rad Scripting Language)"
+  desc "A lightweight, modern CLI tool & scripting language that's familiar, clean, and readable."
   homepage "https://github.com/amterp/rad"
   url "https://github.com/amterp/rad/archive/0.5.29.tar.gz"
   sha256 "b6955d4aa4e9d33a27ba1a941b5645bebf15a1ad047b3e6de45e94c92172417f"
