f9ce0aea86752fbf0543893a66a8a554acf04535
Alexander Terp <alexander.terp@gmail.com>
2025-05-04 06:48:43 +0000

    rad 0.5.33

--- rad.rb
+++ rad.rb
@@ -1,8 +1,8 @@
 class Rad < Formula
   desc "A lightweight, modern CLI tool & scripting language that's familiar, clean, and readable."
   homepage "https://github.com/amterp/rad"
-  url "https://github.com/amterp/rad/archive/0.5.32.tar.gz"
-  sha256 "349150a738847ce2b8b60ceab4745d48d680a675bd1996d0576930bd77c9d442"
+  url "https://github.com/amterp/rad/archive/0.5.33.tar.gz"
+  sha256 "f0c10f698b1395dc1daa2d85aef05ca4f9897e8781ac078c026ab984a77273ef"
 
   depends_on "go" => :build
 
